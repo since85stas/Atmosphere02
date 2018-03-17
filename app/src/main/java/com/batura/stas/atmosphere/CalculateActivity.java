b3559ea8e9ad3965da89f19a0fc9f5184dcf5842
Stas <seeyou85@gmail.com>
2018-03-17 03:16:38 +0300

    Added calculation activity

---
+++ app/src/main/java/com/batura/stas/atmosphere/CalculateActivity.java
@@ -0,0 +1,13 @@
+package com.batura.stas.atmosphere;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculate_activity);
+    }
+}
--- app/src/main/java/com/batura/stas/atmosphere/MainActivity.java
+++ app/src/main/java/com/batura/stas/atmosphere/MainActivity.java
@@ -2,12 +2,24 @@ package com.batura.stas.atmosphere;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView calculateTextView = (TextView) findViewById(R.id.calculate_text);
+        calculateTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.calculate_activity);
+            }
+        });
     }
 }
+
+
