11180f28702c7fd3afd4dc4dfea7468fd5a5593d
seeyou85stas <seeyou85@gmail.com>
2018-03-29 12:16:23 +0300

    Added button icons and formatten dimensions values

--- app/src/main/java/com/batura/stas/atmosphere/CalculateActivity.java
+++ app/src/main/java/com/batura/stas/atmosphere/CalculateActivity.java
@@ -2,6 +2,10 @@ package com.batura.stas.atmosphere;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CalculateActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class CalculateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calculate_activity);
+
+        Button calculateButton = findViewById(R.id.calculateButtonIn);
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText altitudeValueText = findViewById(R.id.altitudeValue);
+                EditText machValueText     = findViewById(R.id.machValue);
+                if (machValueText.getText().length() == 0) {
+                    if (altitudeValueText.getText().length() == 0) {
+                        Toast toast01 = Toast.makeText(getApplicationContext(),"No input parameters",Toast.LENGTH_SHORT);
+                        toast01.show();
+                    }
+                }
+
+            }
+        });
     }
 }
--- app/src/main/java/com/batura/stas/atmosphere/MainActivity.java
+++ app/src/main/java/com/batura/stas/atmosphere/MainActivity.java
@@ -13,8 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //open main activity
-
         //Atmosphere atm = new Atmosphere();
         //double cpTest = atm.findEnthalpy(300);
         TextView calculateTextView = (TextView) findViewById(R.id.calculate_text);
